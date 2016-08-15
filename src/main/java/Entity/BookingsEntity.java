e6503c1ed24f654830d8d20901d2be2fc0d98f9c
Vlad Artyomov <artyomov.dev@gmail.com>
2016-08-15 12:06:57 +0300

    Added Hibernate and MySQL integration

---
+++ src/main/java/Entity/BookingsEntity.java
@@ -0,0 +1,67 @@
+package Entity;
+
+import javax.persistence.*;
+import java.sql.Date;
+
+/**
+ * Created by Vlad on 15.08.2016.
+ */
+@Entity
+@Table(name = "bookings", schema = "booking_calendar")
+public class BookingsEntity {
+    private int id;
+    private Date dateFrom;
+    private Date dateTo;
+
+    @Id
+    @Column(name = "id")
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Basic
+    @Column(name = "date_from")
+    public Date getDateFrom() {
+        return dateFrom;
+    }
+
+    public void setDateFrom(Date dateFrom) {
+        this.dateFrom = dateFrom;
+    }
+
+    @Basic
+    @Column(name = "date_to")
+    public Date getDateTo() {
+        return dateTo;
+    }
+
+    public void setDateTo(Date dateTo) {
+        this.dateTo = dateTo;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        BookingsEntity that = (BookingsEntity) o;
+
+        if (id != that.id) return false;
+        if (dateFrom != null ? !dateFrom.equals(that.dateFrom) : that.dateFrom != null) return false;
+        if (dateTo != null ? !dateTo.equals(that.dateTo) : that.dateTo != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id;
+        result = 31 * result + (dateFrom != null ? dateFrom.hashCode() : 0);
+        result = 31 * result + (dateTo != null ? dateTo.hashCode() : 0);
+        return result;
+    }
+}
